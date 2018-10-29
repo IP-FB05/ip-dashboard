d645e767ecc11f3a30dbaaaa3d7bf570b03ba3f1
Furkan Kocatürk <mcfurkan94@gmail.com>
2018-10-29 16:19:14 +0100

    Added Login Component (Dummy)

--- dash-app/mydash/src/app/app.module.ts
+++ dash-app/mydash/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { ProcessesComponent } from './processes/processes.component';
 
 import { FormsModule } from '@angular/forms';
-import { MenuComponent } from './menu/menu.component'; // <-- NgModel lives here
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     ProcessesComponent,
-    MenuComponent
+    MenuComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
