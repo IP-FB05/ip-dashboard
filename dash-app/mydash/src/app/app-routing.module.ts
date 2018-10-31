49c7553edb601e68d9e2e27637308100005d3f1e
canerto <caner.togan@alumni.fh-aachen.de>
2018-10-31 20:22:10 +0100

    Systeme anzeigen und Prozesse anzeigen

--- dash-app/mydash/src/app/app-routing.module.ts
+++ dash-app/mydash/src/app/app-routing.module.ts
@@ -4,13 +4,16 @@ import { ProcessesComponent } from './processes/processes.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { ProcessDetailComponent }  from './process-detail/process-detail.component';
 import { LoginComponent } from './login/login.component'; 
+import { SystemsComponent } from './systems/systems.component';
 
 const routes: Routes = [
  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
  { path: 'dashboard', component: DashboardComponent },
  { path: 'detail/:id', component: ProcessDetailComponent },
  { path: 'login', component: LoginComponent },
- { path: 'processes', component: ProcessesComponent }
+ { path: 'processes', component: ProcessesComponent },
+ { path: 'systems', component: SystemsComponent }
+
 ];
 
 @NgModule({
--- dash-app/mydash/src/app/systems/systems.component.ts
+++ dash-app/mydash/src/app/systems/systems.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-systems',
   templateUrl: './systems.component.html',
