7a7f16b6b744b2016a6feeb55aa71fce9ed6938b
Mario Trappein <34215934+Moonprayer@users.noreply.github.com>
2018-11-11 16:42:19 +0100

    Added delete & create to API

--- dashboard api/api/objects/system.php
+++ dashboard api/api/objects/system.php
@@ -28,6 +28,56 @@ class System{
     $stmt->execute();
  
     return $stmt;
-}
+	}
+	// create system
+	function create(){
+	 
+		// query to insert record
+		$query = "INSERT INTO systeme SET name=:name, beschreibung=:beschreibung, link=:link";
+	 
+		// prepare query
+		$stmt = $this->conn->prepare($query);
+	 
+		// sanitize
+		$this->name=htmlspecialchars(strip_tags($this->name));
+		$this->beschreibung=htmlspecialchars(strip_tags($this->beschreibung));
+		$this->link=htmlspecialchars(strip_tags($this->link));
+	 
+		// bind values
+		$stmt->bindParam(":name", $this->name);
+		$stmt->bindParam(":beschreibung", $this->beschreibung);
+		$stmt->bindParam(":link", $this->link);
+	 
+		// execute query
+		if($stmt->execute()){
+			return true;
+		}
+	 
+		return false;
+	}
+	
+	// delete the system
+	function delete(){
+ 
+    // delete query
+    $query = "DELETE FROM " . $this->table_name . " WHERE systemID = ?";
+ 
+    // prepare query
+    $stmt = $this->conn->prepare($query);
+ 
+    // sanitize
+    $this->systemID=htmlspecialchars(strip_tags($this->systemID));
+ 
+    // bind id of record to delete
+    $stmt->bindParam(1, $this->systemID);
+ 
+    // execute query
+    if($stmt->execute()){
+        return true;
+    }
+ 
+    return false;
+     
+	}
 }
 ?>
