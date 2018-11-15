df84444f4a388da07742ce76e41a6bd4e0da5f88
Furkan Kocatürk <mcfurkan94@gmail.com>
2018-11-15 14:03:37 +0100

    test App angelegt

--- dash-app/mydash/src/app/login/login.component.ts
+++ dash-app/mydash/src/app/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,30 +7,7 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
-  form: FormGroup;
-
-  constructor(private fb: FormBuilder,
-    private authService: AuthService,
-    private router: Router) {
-    this.form = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-  
-  login() {
-    const val = this.form.value;
-
-    if (val.email && val.password) {
-      this.authService.login(val.email, val.password)
-        .subscribe(
-          () => {
-            console.log("User is logged in");
-            this.router.navigateByUrl('/');
-          }
-        );
-    }
-  }
+  constructor() { }
 
   ngOnInit() {
   }
