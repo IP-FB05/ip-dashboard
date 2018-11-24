f9f694ac559c213659689e8f224702fddc220415
canerto <caner.togan@alumni.fh-aachen.de>
2018-11-24 16:40:59 +0100

    Fixed Search-Filter

--- dash-app/mydash/src/app/filter-documents.pipe.ts
+++ dash-app/mydash/src/app/filter-documents.pipe.ts
@@ -12,6 +12,6 @@ export class filterDocumentsPipe implements PipeTransform {
     transform(documents: Document[], text: string): Document[] {
 
         if (text == null || text === "") return documents;
-        return documents.filter(d => d.name.toLowerCase().includes(text) || d.categoryname.toLowerCase().includes(text));
+        return documents.filter(d => d.name.includes(text) || d.categoryname.includes(text) || d.name.toLowerCase().includes(text) || d.categoryname.toLowerCase().includes(text));
     }
 }
