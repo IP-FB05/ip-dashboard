f095a39b1357e7b3d285f58d7b84ff334d098e4e
Mario Trappein <34215934+Moonprayer@users.noreply.github.com>
2018-12-04 16:23:50 +0100

    CORS fix

--- pruefungsamtconnector/src/main/java/controller/SecurityConfig.java
+++ pruefungsamtconnector/src/main/java/controller/SecurityConfig.java
@@ -5,8 +5,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import utils.Config;
 
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.Arrays;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+import utils.Config;
+/*
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	@Override
@@ -19,4 +27,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.inMemoryAuthentication().withUser(Config.getConfig(Config.REST_USER)).password("{noop}" + Config.getConfig(Config.REST_PASS)).roles("USER");
 	}
+}*/
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            // by default uses a Bean by the name of corsConfigurationSource
+            .cors().and()
+            .authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
+    }
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser(Config.getConfig(Config.REST_USER)).password("{noop}" + Config.getConfig(Config.REST_PASS)).roles("USER");
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","DELETE","PUT","OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
