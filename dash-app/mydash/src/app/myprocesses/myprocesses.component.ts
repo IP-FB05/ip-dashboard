a79e5ca90c956ef7d95ca10911a1f6b770b77f43
Mario Trappein <34215934+Moonprayer@users.noreply.github.com>
2018-12-16 23:44:21 +0100

    Laufende Prozesse

--- dash-app/mydash/src/app/myprocesses/myprocesses.component.ts
+++ dash-app/mydash/src/app/myprocesses/myprocesses.component.ts
@@ -1,4 +1,94 @@
 import { Component, OnInit } from '@angular/core';
+import 'camunda-bpm-sdk-js';
+import 'jquery';
+
+declare var CamSDK: any;
+
+var $formContainer = $('.column.right');
+
+var camClient = new CamSDK.Client({
+  mock: false,
+  apiUri: 'http://localhost:8080/engine-rest'
+});
+
+var taskService = new camClient.resource('task');
+
+function loadTasks() {
+  // fetch the list of available tasks
+  taskService.list({
+    // filter?
+  }, function (err, results) {
+    if (err) {
+      throw err;
+    }
+    showTasks(results);
+  });
+}
+
+function showTasks(results) {
+  // generate the HTML for the list of tasks
+  var items = [];
+  $.each(results._embedded.task, function (t, task) {
+    items = items.concat([
+      '<li data-task-id="', task.id, '">',
+      task.name || task.id,
+      task.description ? '<div class="description">' : '',
+      task.description,
+      task.description ? '</div>' : '',
+      '</li>'
+    ]);
+  });
+
+  $('#tasks')
+    // add the HTML to the list
+    .html(items.join(''))
+
+    // attach click events to the task list items
+    .find('> li').on("click", function () {
+
+      // load the the task form (getting the task ID from the tag attribute)
+      loadTaskForm($(this).attr('data-task-id'), function(err, camForm) {
+        if (err) {
+          throw err;
+        }
+
+        var $submitBtn = $('<button type="submit">Complete</button>').click(function () {
+          camForm.submit(function (err) {
+            if (err) {
+              throw err;
+            }
+
+            // clear the form
+            $formContainer.html('');
+
+            loadTasks();
+          });
+        });
+
+        camForm.containerElement.append($submitBtn);
+      });
+    });
+}
+
+
+
+function loadTaskForm(taskId, callback) {
+  // loads the task form using the task ID provided
+  taskService.form(taskId, function(err, taskFormInfo) {
+    var url = "http://localhost:8080" + taskFormInfo.key.replace('embedded:app:', taskFormInfo.contextPath + '/');
+
+    new CamSDK.Form({
+      client: camClient,
+      formUrl: url,
+      taskId: taskId,
+      containerElement: $formContainer,
+
+      // continue the logic with the callback
+      done: callback
+    });
+  });
+}
+
 
 @Component({
   selector: 'app-myprocesses',
@@ -7,9 +97,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprocessesComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit() {
+    // load the tasks at start
+    loadTasks();
   }
 
 }
