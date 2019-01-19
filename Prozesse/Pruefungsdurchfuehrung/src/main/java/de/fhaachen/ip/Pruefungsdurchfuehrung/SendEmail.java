5336cfe14bac04049e262bbf6ed5a49c73c10512
philipp <echo128@web.de>
2019-01-19 23:01:59 +0100

    standardize bpmn

--- Prozesse/Pruefungsdurchfuehrung/src/main/java/de/fhaachen/ip/Pruefungsdurchfuehrung/SendEmail.java
+++ Prozesse/Pruefungsdurchfuehrung/src/main/java/de/fhaachen/ip/Pruefungsdurchfuehrung/SendEmail.java
@@ -1,6 +1,5 @@
 package de.fhaachen.ip.Pruefungsdurchfuehrung;
 
-
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -18,13 +17,11 @@ import org.camunda.bpm.engine.delegate.JavaDelegate;
 
 import utils.Config;
 
-
-
 public class SendEmail implements JavaDelegate {
 
-    public void execute(DelegateExecution execution) throws Exception {
-	
-    	final String username = "dashboarddonotreply@gmail.com";
+	public void execute(DelegateExecution execution) throws Exception {
+
+		final String username = "dashboarddonotreply@gmail.com";
 		final String password = Config.getConfig(Config.MAIL_PASS);
 
 		Properties props = new Properties();
@@ -33,54 +30,50 @@ public class SendEmail implements JavaDelegate {
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.port", "587");
 
-		Session session = Session.getInstance(props, new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication(username, password);
+		Session session = Session.getInstance(props,
+			new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
 			}
-		});
+		);
 
-        try {
-
-        	Message message = new MimeMessage(session);
+		try {
+			Message message = new MimeMessage(session);
 			message.setFrom(new InternetAddress("dashboarddonotreply@gmail.com"));
-			
-			
+
 			String subject = execution.getVariable("subject").toString();
-            message.setSubject(subject);
+			message.setSubject(subject);
 
-            String text = execution.getVariable("text").toString();
-            message.setText(text);
-			
+			String text = execution.getVariable("text").toString();
+			message.setText(text);
 
-            //Get zielEmail
-            
+			//Get zielEmail
 			String[][] students = (String[][]) execution.getVariable("studRESTRe");
-            
-            String destination = "";
-            
+
+			String destination = "";
+
 			for(int i = 0; i < students.length; i++) {
-				
 				destination = students[i][2].toString();
 				message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
-				
-            	//Transport.send(message);
-            }
-			
-			String starterstudent = (String) execution.getVariable("initiator");  
-            
-            ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();            
-            
-            destination = processEngine.getIdentityService().createUserQuery()
-					.userId(starterstudent).singleResult().getEmail();
-            
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
-            
-            //Transport.send(message);
 
-			
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+				//Transport.send(message);
+			}
+
+			String starterstudent = (String) execution.getVariable("initiator");  
+
+			ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();            
+
+			destination = processEngine.getIdentityService().createUserQuery()
+				.userId(starterstudent).singleResult().getEmail();
+
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
+
+			//Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
