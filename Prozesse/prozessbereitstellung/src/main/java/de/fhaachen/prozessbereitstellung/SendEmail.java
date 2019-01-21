4bd55efba1314483fe42ef4946412295f9189602
philipp <echo128@web.de>
2019-01-21 03:07:22 +0100

    standardize prozessbereitstellung

--- Prozesse/prozessbereitstellung/src/main/java/de/fhaachen/prozessbereitstellung/SendEmail.java
+++ Prozesse/prozessbereitstellung/src/main/java/de/fhaachen/prozessbereitstellung/SendEmail.java
@@ -1,53 +1,69 @@
 package de.fhaachen.prozessbereitstellung;
 
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-import utils.*;
-import java.util.Properties;
-import javax.mail.*;
-import javax.mail.internet.*;
+import utils.Config;
 
 public class SendEmail implements JavaDelegate {
 
-    public void execute(DelegateExecution execution) throws Exception {
+	public void execute(DelegateExecution execution) throws Exception {
 
-    	final String username = "dashboarddonotreply@gmail.com";
-        final String password = Config.getConfig(Config.MAIL_PASS);
+		final String username = "dashboarddonotreply@gmail.com";
+		final String password = Config.getConfig(Config.MAIL_PASS);
 
-        Properties props = new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props,
-                new Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
+		Session session = Session.getInstance(props,
+			new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			}
+		);
 
-        try {
+		try {
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("dashboarddonotreply@gmail.com"));
 
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("dashboarddonotreply@gmail.com"));
+			//Get zielEmail
+			//starter name
+			String starterstudent = (String) execution.getVariable("initiator");  
 
-            String ziel = execution.getProcessEngineServices().getIdentityService().createUserQuery().userId((String) execution.getVariable("user")).singleResult().getEmail();
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(ziel));
+			ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();            
 
-            String subject = execution.getVariable("subject").toString();
-            message.setSubject(subject);
+			String destination = processEngine.getIdentityService().createUserQuery()
+				.userId(starterstudent).singleResult().getEmail();
 
-            String text = execution.getVariable("text").toString();
-            message.setText(text);
+			message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(destination));
 
-            Transport.send(message);
+			String subject = execution.getVariable("subject").toString();
+			message.setSubject(subject);
 
+			String text = execution.getVariable("text").toString();
+			message.setText(text);
 
-        } catch (MessagingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+			//Transport.send(message);
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 }
