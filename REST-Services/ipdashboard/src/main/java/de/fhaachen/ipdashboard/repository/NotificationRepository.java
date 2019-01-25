500a636dff8c3b565641b8dde8447c40eeea4178
canerto <caner.togan@alumni.fh-aachen.de>
2019-01-25 22:00:20 +0100

    Change Rest

--- REST-Services/ipdashboard/src/main/java/de/fhaachen/ipdashboard/repository/NotificationRepository.java
+++ REST-Services/ipdashboard/src/main/java/de/fhaachen/ipdashboard/repository/NotificationRepository.java
@@ -4,10 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import javax.transaction.Transactional;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import de.fhaachen.ipdashboard.model.Notification;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
