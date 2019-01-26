91e09e609a4cbbb9eb86fbc53305bcdad0cc500e
Jana <Jana@149.201.225.134>
2019-01-27 00:56:20 +0100

    bugfix camunda authentications

--- Prozesse/prozessbereitstellung/src/main/java/de/fhaachen/prozessbereitstellung/DBAuthentication.java
+++ Prozesse/prozessbereitstellung/src/main/java/de/fhaachen/prozessbereitstellung/DBAuthentication.java
@@ -7,42 +7,38 @@ import org.camunda.bpm.engine.authorization.Resources;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-<<<<<<< HEAD
 import java.util.List;
-=======
-import java.util.ArrayList;
-import java.util.Collection;
-
->>>>>>> branch 'master' of https://github.com/IP-FB05/ip-dashboard.git
 
 public class DBAuthentication implements JavaDelegate {
 
+	public void execute(DelegateExecution execution) {
 
-    public void execute(DelegateExecution execution){                            
+		// get Authentication Service
+		AuthorizationService authService = execution.getProcessEngineServices().getAuthorizationService();
 
-			// get Authentication Service
-			AuthorizationService authService = execution.getProcessEngineServices().getAuthorizationService();
-			
-			// get groups
-			@SuppressWarnings("unchecked")
-			List<String> groups = (List<String>) execution.getVariable("groups");
-			Authorization newAuth;
-			
-			String groupString = "";
-			
-			for (String group : groups) {
+		// get groups
+		@SuppressWarnings("unchecked")
+		List<String> groups = (List<String>) execution.getVariable("groups");
+		Authorization newAuth;
+
+		String groupString = "";
+
+		for (String group : groups) {
+			if (authService.createAuthorizationQuery().resourceId((String) execution.getVariable("definitionId"))
+					.groupIdIn(group).list().size() == 0) {
+
 				// create Authorization
 				newAuth = authService.createNewAuthorization(Authorization.AUTH_TYPE_GRANT);
-				
+
 				// set Resource Type
 				newAuth.setResourceType(Resources.PROCESS_DEFINITION.resourceType());
 
 				// set Process as Ressource
 				newAuth.setResourceId((String) execution.getVariable("definitionId"));
-				
+
 				// set group
 				newAuth.setGroupId(group);
-				
+
 				// add permissions
 //				newAuth.addPermission(Permissions.READ_INSTANCE);
 //				newAuth.addPermission(Permissions.UPDATE_INSTANCE);
@@ -50,22 +46,23 @@ public class DBAuthentication implements JavaDelegate {
 //				newAuth.addPermission(Permissions.TASK_WORK);
 //				newAuth.addPermission(Permissions.UPDATE_TASK);
 				newAuth.addPermission(Permissions.ALL);
-				
+
 				// save Authorization
 				authService.saveAuthorization(newAuth);
-				
-				groupString = groupString + group + ",";
-				
 			}
 			
-			if(!groupString.isEmpty()) {
-				groupString.substring(0, groupString.length() - 1);
-			}
-			else {
-				groupString = "admin,gast,mitarbeiter,professor,pruefungsamt,student";
-			}
-			
-			execution.setVariable("groupString", groupString);
+			groupString = groupString + group + ",";
+
+
+		}
+
+		if (!groupString.isEmpty()) {
+			groupString.substring(0, groupString.length() - 1);
+		} else {
+			groupString = "admin,gast,mitarbeiter,professor,pruefungsamt,student";
+		}
+
+		execution.setVariable("groupString", groupString);
 
 	}
 
