268693f3cd55cf4c0fc9c4e6645bbf7ceb7970ca
LangeFlorian <florian.lange1@alumni.fh-aachen.de>
2019-01-28 14:01:25 +0100

    some final features and comments

--- Prozesse/kapazitaetsplanung/src/main/java/de/fhaachen/ip/kapazitaetsplanung/placeholder.java
+++ Prozesse/kapazitaetsplanung/src/main/java/de/fhaachen/ip/kapazitaetsplanung/placeholder.java
@@ -3,7 +3,7 @@ package de.fhaachen.ip.kapazitaetsplanung;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 
-public class olaceholder implements JavaDelegate {
+public class placeholder implements JavaDelegate {
 
     public void execute(DelegateExecution execution) {
 
